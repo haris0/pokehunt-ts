74d00b796837c8ad51952398b279a40bd1988d40
Hari Surrisyad <hari.surrisyad@gmail.com>
2022-01-13 15:43:31 +0700

    setting up react router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { lazy, Suspense } from 'react';
+import {
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+const HomePage = lazy(() => import('./components/HomePage/HomePage'));
+const Page404 = lazy(() => import('./components/Page404/Page404'));
+
+const routes = [
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/404',
+    element: <Page404 />,
+  },
+];
+
 const App = () => (
-  <div className="App">
-    New World
-  </div>
+  <Routes>
+    {routes.map((router) => (
+      <Route
+        key={router.path}
+        path={router.path}
+        element={(
+          <Suspense fallback={<div />}>
+            {router.element}
+          </Suspense>
+        )}
+      />
+    ))}
+    <Route path="*" element={<Navigate replace to="/404" />} />
+  </Routes>
 );
 
 export default App;
